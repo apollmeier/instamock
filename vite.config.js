9b8840d017c8a78e4298003dac043649f0634fbe
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-11 13:12:18 +0100

    Add new icons and update package dependencies
    Introduced multiple SVG icons, including search, home, and create variants. Updated package-lock.json to add new dependencies like "unplugin-icons" and supporting modules for asset optimizations and integrations.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
+import {FileSystemIconLoader} from "unplugin-icons/loaders";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: 'svelte',
+			customCollections: {
+				'instamock': FileSystemIconLoader(
+					'./src/lib/assets/icons',
+				)
+			}
+		})
+	]
 });
