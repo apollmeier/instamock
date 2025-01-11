2b9faa1a8a9a230d78336ac83ed645e056ab16d5
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-11 13:14:59 +0100

    Add preview page with Device and TabBar components
    Introduced a new preview page to display the Device and TabBar components. This page sets up a centered layout for presenting these components effectively.

---
+++ src/routes/preview/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Device from '$lib/components/Device/Device.svelte';
+    import TabBar from "$lib/components/TabBar/TabBar.svelte";
+
+
+</script>
+
+<section class="min-h-screen flex justify-center items-center">
+    <Device>
+        <TabBar />
+    </Device>
+</section>
