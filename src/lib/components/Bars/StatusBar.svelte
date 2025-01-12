c14e0f7c22a2a01a27daa4a167d81696a67e8ada
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-12 16:43:51 +0100

    Add StatusBar component to preview page
    Introduced a new StatusBar component that displays the current time and updates every second. Integrated the StatusBar into the preview page's layout alongside the existing TabBar.

---
+++ src/lib/components/Bars/StatusBar.svelte
@@ -0,0 +1,26 @@
+<script>
+    import {onMount} from "svelte";
+
+    let currentDateTime = new Date();
+
+    onMount(() => {
+        const interval = setInterval(() => {
+            currentDateTime = new Date();
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    });
+
+    function getHour(c) {
+        return c.getHours();
+    }
+    function getMinute(c) {
+        return c.getMinutes() < 10 ? "0" + c.getMinutes() : c.getMinutes();
+    }
+</script>
+
+<section class="p-4 pb-2 flex flex-row">
+    <time class="font-semibold">{getHour(currentDateTime)}:{getMinute(currentDateTime)}</time>
+</section>
--- src/routes/preview/+page.svelte
+++ src/routes/preview/+page.svelte
@@ -4,10 +4,12 @@
 
 
     import TabBar from "$lib/components/Bars/TabBar.svelte";
+    import StatusBar from "$lib/components/Bars/StatusBar.svelte";
 </script>
 
 <section class="min-h-screen flex justify-center items-center">
     <Device>
+        <StatusBar />
         <TabBar />
     </Device>
 </section>
