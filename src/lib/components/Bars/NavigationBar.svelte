52eb6d6da5516d58c54f3587720ae5fccd78bcac
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-12 16:44:04 +0100

    Add NavigationBar component to preview page
    Introduced a new NavigationBar component and integrated it into the preview page layout. The NavigationBar includes icons for liking and sharing with badge indicators, enhancing the UI and interactivity.

---
+++ src/lib/components/Bars/NavigationBar.svelte
@@ -0,0 +1,17 @@
+<script>
+    import IconLike from '~icons/instamock24/Like'
+    import IconShare from '~icons/instamock24/Share'
+    import IconChevronDown from '~icons/instamock16/ChevronDown'
+</script>
+
+<section class="px-4 py-2 flex flex-row items-center gap-5">
+    <h2 class="text-xl font-bold mr-auto">For you <IconChevronDown class="inline-block"/></h2>
+    <div class="relative">
+        <IconLike/>
+        <span class="absolute top-0 right-0 size-2 rounded-full bg-red-100 ring-2 ring-white"></span>
+    </div>
+    <div class="relative">
+        <IconShare/>
+        <span class="absolute top-0 right-0 translate-x-1/3 -translate-y-1/3 rounded-full px-1.5 bg-red-100 text-[10px] text-white font-bold">5</span>
+    </div>
+</section>
--- src/routes/preview/+page.svelte
+++ src/routes/preview/+page.svelte
@@ -5,11 +5,15 @@
 
     import TabBar from "$lib/components/Bars/TabBar.svelte";
     import StatusBar from "$lib/components/Bars/StatusBar.svelte";
+    import NavigationBar from "$lib/components/Bars/NavigationBar.svelte";
 </script>
 
 <section class="min-h-screen flex justify-center items-center">
     <Device>
         <StatusBar />
+        <div>
+            <NavigationBar />
+        </div>
         <TabBar />
     </Device>
 </section>
