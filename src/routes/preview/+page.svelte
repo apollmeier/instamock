ba02f37e436969bf604f9509953fc861b6a969a8
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-12 16:42:03 +0100

    Rename and relocate TabBar component for better organization.
    The TabBar component was moved from `components/TabBar` to `components/Bars` to align with the folder's naming convention. References to the component were updated accordingly to ensure functionality remains intact.

--- src/routes/preview/+page.svelte
+++ src/routes/preview/+page.svelte
@@ -3,6 +3,7 @@
     import TabBar from "$lib/components/TabBar/TabBar.svelte";
 
 
+    import TabBar from "$lib/components/Bars/TabBar.svelte";
 </script>
 
 <section class="min-h-screen flex justify-center items-center">
