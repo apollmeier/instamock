aec04fed0ebb4c4269f046f9126b37331d390499
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-18 14:05:12 +0100

    Adjust Device aspect ratio to 9/20.
    Updated the aspect ratio of the Device component container from 9/16 to 9/20.

--- src/lib/components/Device/Device.svelte
+++ src/lib/components/Device/Device.svelte
@@ -2,6 +2,6 @@
     let {children} = $props();
 </script>
 
-<div class="max-w-[375px] w-full aspect-[9/16] border-4 border-black rounded-2xl overflow-hidden relative box-content">
+<div class="max-w-[375px] w-full aspect-[9/20] border-4 border-black rounded-2xl overflow-hidden relative box-content">
     {@render children()}
 </div>
