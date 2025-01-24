521226907713844d2303b97cd35605518d8f01ec
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-24 19:27:02 +0100

    Add Stories component with avatars and story indicators
    Introduce a new `Stories` component to display a list of avatars with visual story status indicators using the `StoryRing` component. The component includes styles for proper layout and responsiveness. This feature sets up the foundation for displaying user stories in the interface.

---
+++ src/lib/components/Stories/Stories.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Avatar from "$lib/components/Avatar/Avatar.svelte";
+    import StoryRing from "$lib/components/Avatar/StoryRing.svelte";
+</script>
+
+<section class="px-1.5 pt-2 pb-4">
+    <ul class="flex gap-5">
+        <li>
+            <Avatar src="https://picsum.photos/128/128"/>
+        </li>
+        <li>
+            <Avatar src="https://picsum.photos/128/128">
+                <StoryRing width="3px" gap="2px" status="close-friend"/>
+            </Avatar>
+        </li>
+        <li>
+            <Avatar src="https://picsum.photos/128/128">
+                <StoryRing width="3px" gap="2px" status="new"/>
+            </Avatar>
+        </li>
+        <li>
+            <Avatar src="https://picsum.photos/128/128">
+                <StoryRing width="3px" gap="2px" status="new"/>
+            </Avatar>
+        </li>
+        <li>
+            <Avatar src="https://picsum.photos/128/128">
+                <StoryRing width="3px" gap="2px" status="read"/>
+            </Avatar>
+        </li>
+    </ul>
+</section>
+
+<style>
+    ul > li {
+        flex-shrink: 0;
+    }
+</style>
