1da2cb5bd88fd453b4c89b95c8dde2da742dd3a1
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-24 19:26:37 +0100

    Refactor Avatar component to improve structure and clarity
    Replaced generic props handling with destructured properties for better readability and maintainability. Adjusted styling and structure to ensure proper rendering of children and consistent layout. This change simplifies the logic while preserving existing functionality.

--- src/lib/components/Avatar/Avatar.svelte
+++ src/lib/components/Avatar/Avatar.svelte
@@ -1,7 +1,16 @@
 <script>
-    let props = $props();
+    const {
+        src,
+        add,
+        children,
+    } = $props();
 </script>
 
-<div class={['rounded-full overflow-hidden size-5', props.class]}>
-    {@render props?.children()}
+<div>
+    <div class="relative flex justify-center items-center">
+        <img {src} class="rounded-full aspect-square size-[6]" alt="">
+
+        {@render children?.()}
+    </div>
+
 </div>
