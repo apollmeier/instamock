36ff242c53328b7faa719e7b91a759812073a317
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-24 19:26:48 +0100

    Add StoryRing component for Avatar status visualization
    This component provides a visual indicator around an Avatar to represent user status (e.g., 'read', 'new', or 'close-friend'). It dynamically adjusts styles based on props like width, gap, and status.

---
+++ src/lib/components/Avatar/StoryRing.svelte
@@ -0,0 +1,35 @@
+<script>
+    let  {
+        width,
+        gap,
+        status,
+    } = $props()
+
+    let className;
+
+    switch (status) {
+        case 'read':
+            className = 'bg-gray-30';
+            break;
+        case 'new':
+            className = 'bg-gradient-instagram'
+            break;
+        case 'close-friend':
+            className = 'bg-green-100'
+    }
+
+</script>
+
+<div class={["absolute -z-10 rounded-full flex justify-center items-center aspect-square", className]} style="--story-ring-width: {width}; --story-ring-gap: {gap}">
+    <div class="rounded-full aspect-square bg-white"></div>
+</div>
+
+<style>
+    div {
+        width: calc(100% + (var(--story-ring-width) + var(--story-ring-gap)) * 2);
+
+        div {
+            width: calc(100% - var(--story-ring-width) * 2);
+        }
+    }
+</style>
