724fb6347fd2333622f7771305b3e86794343acc
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-25 22:03:17 +0100

    Set consistent Avatar sizing and remove redundant styles
    Updated all Avatar components to use the `size="size-17"` property for uniform sizing. Removed the redundant inline `shrink` style by incorporating it directly in the class. Simplifies maintenance and ensures consistent layout behavior.

--- src/lib/components/Stories/Stories.svelte
+++ src/lib/components/Stories/Stories.svelte
@@ -4,35 +4,29 @@
 </script>
 
 <section class="px-1.5 pt-2 pb-4">
-    <ul class="flex gap-5">
+    <ul class="flex gap-5 *:shrink-0">
         <li>
-            <Avatar src="https://picsum.photos/128/128"/>
+            <Avatar src="https://picsum.photos/128/128" size="size-17" />
         </li>
         <li>
-            <Avatar src="https://picsum.photos/128/128">
+            <Avatar src="https://picsum.photos/128/128" size="size-17">
                 <StoryRing width="3px" gap="2px" status="close-friend"/>
             </Avatar>
         </li>
         <li>
-            <Avatar src="https://picsum.photos/128/128">
+            <Avatar src="https://picsum.photos/128/128" size="size-17">
                 <StoryRing width="3px" gap="2px" status="new"/>
             </Avatar>
         </li>
         <li>
-            <Avatar src="https://picsum.photos/128/128">
+            <Avatar src="https://picsum.photos/128/128" size="size-17">
                 <StoryRing width="3px" gap="2px" status="new"/>
             </Avatar>
         </li>
         <li>
-            <Avatar src="https://picsum.photos/128/128">
+            <Avatar src="https://picsum.photos/128/128" size="size-17">
                 <StoryRing width="3px" gap="2px" status="read"/>
             </Avatar>
         </li>
     </ul>
 </section>
-
-<style>
-    ul > li {
-        shrink: 0;
-    }
-</style>
