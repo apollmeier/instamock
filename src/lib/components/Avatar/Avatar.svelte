c3b4e9b0f3f052faf2703ad4468a250402288a3c
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-26 00:48:53 +0100

    Enhance Avatar component with 'Add' icon and dynamic sizing
    Added support for an optional 'Add' icon to the Avatar component and updated the size property for improved customization. Introduced CSS variables for dynamic styling, making it easier to adjust size and placement. This update improves flexibility and usability of the component.

--- src/lib/components/Avatar/Avatar.svelte
+++ src/lib/components/Avatar/Avatar.svelte
@@ -1,17 +1,39 @@
 <script>
+    import Add from "$lib/components/Avatar/Add.svelte";
+
     const {
         src,
-        size = 'size-6',
+        size = '6',
         add,
         children,
     } = $props();
 </script>
 
 <div>
-    <div class="relative flex justify-center items-center">
-        <img {src} class="rounded-full aspect-square {size}" alt="">
+    <div class="relative flex justify-center items-center" style="--size: {size}">
+        <img {src} class="rounded-full aspect-square">
+
+        {#if add}
+        <div class="add absolute">
+            <Add />
+        </div>
+        {/if}
 
         {@render children?.()}
     </div>
-
 </div>
+
+<style>
+    img {
+        width: calc(var(--spacing) * var(--size));
+    }
+
+    .add {
+        --radius: calc(var(--spacing) * var(--size) / 2);
+        --angle: 315deg;
+
+        translate:
+            calc(cos(var(--angle)) * var(--radius))      /* Translation on X-axis */
+            calc(sin(var(--angle) * -1) * var(--radius)); /* Translation on Y-axis */
+    }
+</style>
