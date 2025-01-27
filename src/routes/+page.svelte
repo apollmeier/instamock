046750d684dd8dddd9d09268eed746fde56faecf
Andre Pollmeier <pollmeier.a@gmail.com>
2025-01-27 20:30:51 +0100

    Update homepage with new design and messaging
    Replace the generic SvelteKit welcome text with a modern, user-focused design. Introduced a new tagline, detailed description, and a CTA button to improve clarity and user engagement.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="min-h-screen content-center">
+    <div class="max-w-5xl mx-auto space-y-12 text-center">
+        <h1 class="text-7xl"><em class="font-bold bg-linear-instagram bg-clip-text text-transparent">The smarter</em> way to perfect your content.</h1>
+        <p class="font-light leading-normal text-2xl text-balance antialiased">InstaMock instantly shows you how your photos will look in the feed - before posting. Preview single shots or carousels, find the perfect crop, and create a cohesive feed without uploading. Zero signup, completely free.</p>
+        <a href="/preview" class="inline-block bg-gray-30 p-4 px-6 rounded-md">Preview your posts</a>
+    </div>
+</section>
