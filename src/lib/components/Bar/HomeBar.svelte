3e598a5bdb3a907006b87c0dd938c21605faac6c
Andre Pollmeier <pollmeier.a@gmail.com>
2025-02-07 21:57:19 +0100

    Move decorative bar to new HomeBar component.
    The decorative bar was extracted from TabBar.svelte and placed into a new HomeBar.svelte component for better modularity and reusability. This change simplifies TabBar while isolating the decorative element.

---
+++ src/lib/components/Bar/HomeBar.svelte
@@ -0,0 +1,3 @@
+<div class="px-6 py-2">
+    <div class="h-[5px] w-36 mx-auto bg-black rounded-full"></div>
+</div>
--- src/lib/components/Bar/TabBar.svelte
+++ src/lib/components/Bar/TabBar.svelte
@@ -27,7 +27,4 @@
             </li>
         </ul>
     </nav>
-    <div class="px-6 py-2">
-        <div class="h-[5px] w-36 mx-auto bg-black rounded-full"></div>
-    </div>
 </section>
